8d723d8aa543d3d202bfc814488acfc3e9083f14
Luís Arteiro <lucho14garfield@gmail.com>
2024-09-13 01:14:42 +0100

    test if it fails.

--- example_script.groovy
+++ example_script.groovy
@@ -1,19 +1,30 @@
 import com.wisecoders.dbs.schema.*
 
-Project proj = new Project("sample", "SqlServer");
-println "Project " + proj.getName()
-// CONNECT TO A LOCAL INSTALLED SQLSERVER, DATABASE 'hr' AS USER sa
-Connector con = ConnectorManager.createConnector("pg_con", "org.postgresql.Driver", "jdbc:postgresql://localhost:5432/testdb", "postgres", "postgres")
-// HERE SPECIFY THE CONNECTION PASSWORD
-con.setPassword("postgres")
-// Sample connection using the JDBC URL
-// ConnectorManager.createConnector("test","MongoDb", "com.wisecoders.dbschema.mongodb.JdbcDriver","mongodb://localhost/tournament", "", "");
+try {
 
-con.importSchemes( proj, "public")
+    Project proj = new Project("sample", "SqlServer");
+    println "Project " + proj.getName()
+    // CONNECT TO A LOCAL INSTALLED SQLSERVER, DATABASE 'hr' AS USER sa
+    Connector con = ConnectorManager.createConnector("pg_con", "org.postgresql.Driver", "jdbc:postgresql://localhost:5432/testdb", "postgres", "postgres")
+    // HERE SPECIFY THE CONNECTION PASSWORD
+    con.setPassword("postgres")
+    // Sample connection using the JDBC URL
+    // ConnectorManager.createConnector("test","MongoDb", "com.wisecoders.dbschema.mongodb.JdbcDriver","mongodb://localhost/tournament", "", "");
 
-for( Schema sch : project.schemas ){
-    out.println( "Schema " + sch.getNameWithCatalog() )
-    for ( Table table : sch.tables ){
-        out.println( "  Table " + table.getName() )
+    con.importSchemes( proj, "public")
+
+    for( Schema sch : project.schemas ){
+        out.println( "Schema " + sch.getNameWithCatalog() )
+        for ( Table table : sch.tables ){
+            out.println( "  Table " + table.getName() )
+        }
     }
+
+} catch (Exception e) {
+    println "Script failed: ${e.message}"
+    e.printStackTrace()
+    System.exit(1)  // Ensure the script exits with a non-zero status on failure
 }
+
+// Explicitly exit with 0 if the script succeeds
+System.exit(0)
