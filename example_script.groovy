b0be0c698deaa41be1526861dcc69a38871683ea
Luís Arteiro <lucho14garfield@gmail.com>
2024-10-23 22:21:25 +0100

    chore: Adding defs.

--- example_script.groovy
+++ example_script.groovy
@@ -5,27 +5,35 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+// Connection parameters
+def conn_alias = "pg"
+def conn_dbmsName = "PostgreSQL"
+def conn_driver = "org.postgresql.Driver"
+def conn_jdbcUrl = "jdbc:postgresql://" + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME")
+def conn_username = "postgres"
+def conn_password = "postgres"
 
 try {
     // Make sure to have the PostgreSQL driver
-    com.wisecoders.dbs.cli.command.connectivity.DownloadDriverCommand.downloadDriver("PostgreSQL")
+    com.wisecoders.dbs.cli.command.connectivity.DownloadDriverCommand.downloadDriver(conn_dbmsName)
 
     // Create sample project
-    Project project = new Project("sample", "PostgreSQL");
+    Project project = new Project("sample", conn_dbmsName);
 
     // Create a connection to the PostgreSQL database
-    //Connector con = ConnectorManager.createConnector("pg_con","PostgreSQL", "localhost", 5432, "org.postgresql.Driver", "postgres", "postgres")
-    //String jdbcUrl = "jdbc:postgresql://" + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME")
-    String jdbcUrl = "jdbc:postgresql://" + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME")
     Connector con = ConnectorManager.createConnector(
-        "pg_con",
-        "PostgreSQL",
-        "org.postgresql.Driver",
-        jdbcUrl,
-        "postgres",
-        "postgres"
+        conn_alias,
+        conn_dbmsName,
+        conn_driver,
+        conn_jdbcUrl,
+        conn_username,
+        conn_password
     )
+
+    // Import schemes
     con.importSchemes( project, "public")
+
+    // Print schemas and tables
     for( Schema sch : project.schemas ){
         out.println( "Schema " + sch.getNameWithCatalog() )
         for ( Table table : sch.tables ){
