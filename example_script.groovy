c6e345ef1bfed3542e229c8a12a335b54f32cee6
Luís Arteiro <lucho14garfield@gmail.com>
2024-10-24 04:08:13 +0100

    feat: Exporting HTML5 documentation.

--- example_script.groovy
+++ example_script.groovy
@@ -1,4 +1,10 @@
+import com.wisecoders.dbs.diagram.model.Callout
+import com.wisecoders.dbs.diagram.model.Depict
+import com.wisecoders.dbs.diagram.model.Group
+import com.wisecoders.dbs.diagram.model.Point
 import com.wisecoders.dbs.schema.*
+import javafx.scene.paint.Color
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,6 +18,18 @@ def conn_driver = "org.postgresql.Driver"
 def conn_jdbcUrl = "jdbc:postgresql://" + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME")
 def conn_username = "postgres"
 def conn_password = "postgres"
+
+// Color maps according to table prefixes
+def backgroundColorMap = [
+        "dim" :"#c4e0f9",
+        "fact" :"#feea9b",
+]
+
+def foregroundColorMap = [
+        "dim" :"#96c8f4", 
+        "fact" :"#fddf69",
+]
+
 
 try {
     // Make sure to have the PostgreSQL driver
@@ -30,16 +48,51 @@ try {
         conn_password
     )
 
-    // Import schemes
-    con.importSchemes( project, "public")
+    // Import "public" schema
+    def dbSchema = "public"
+    Schema schema = con.importSchema(project, dbSchema)
+    println "processing schema: ${schema}"
 
-    // Print schemas and tables
-    for( Schema sch : project.schemas ){
-        out.println( "Schema " + sch.getNameWithCatalog() )
-        for ( Table table : sch.tables ){
-            out.println( "  Table " + table.getName() )
+    // Creating layout and groups
+    Layout layout = project.createLayout("ERD-${schema}")
+    def grps = ["defaultKey": null]
+
+    // Create layout in the project. We are creating a single one.
+    // See https://dbschema.com/documentation/layouts.html for more information,
+    for (Table table: schema.tables){
+
+        // We expect all table name to have a "dim_" or "fact_" prefix...
+        String tableType = table.getName().split('_')[0]
+        table.setComment("COMMENT ON TABLE ${table.getName()}")
+        bgColor = backgroundColorMap.get(tableType)
+        fgColor = foregroundColorMap.get(tableType)
+
+        if(!grps.containsKey(tableType)){
+            Group grp = layout.diagram.createGroup(tableType)
+            grps.put(tableType, grp)
+            println "Add layout group for domain ${tableType}"
         }
+
+        grp = grps.get(tableType)
+
+        Depict depict = layout.attach( table )
+        depict.setComment("COMMENT STRING")
+
+        //depict.hideColumnsIfDepictIsLarge()
+
+        grp.attachDepict( depict )
+        grp.setColor(Color.web(bgColor))
+        println "Add table ${table}"
     }
+    layout.autoArrange()
+
+    // Export HTML5 documentation
+    def output = System.getenv("OUTPUT_PATH") + "/erd_" + dbSchema.toLowerCase() + ".html" 
+    htmlFile = new File(output)
+
+    println "general html file: ${output}"
+    layout.generateHtmlDocumentation( htmlFile, [layout] )
+
 } 
 
 // Ensure the script exits with a non-zero status on failure
